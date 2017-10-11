c98770bc640523cd054d469c3d1b211a613ba18e
liupeng110 <717219917@qq.com>
2017-10-11 11:07:39 +0800

    增加mediaTag的so 及java层引用使用方法

--- app/src/main/java/com/andlp/musicplayer/activity/Activity_Group.java
+++ app/src/main/java/com/andlp/musicplayer/activity/Activity_Group.java
@@ -1,15 +1,14 @@
 package com.andlp.musicplayer.activity;
 
 import android.app.ActivityGroup;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
 
 
 import com.andlp.musicplayer.R;
+import com.sds.android.ttpod.media.MediaTag;
 import com.andlp.musicplayer.util.L;
 
 import org.greenrobot.eventbus.EventBus;
@@ -21,6 +20,7 @@ import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
 
+
 /**
  * 717219917@qq.com  2017/9/27 14:56
  */
@@ -30,8 +30,7 @@ public class Activity_Group extends ActivityGroup{
    @ViewInject(R.id.bottom) Button bottom ;
     View myview;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         x.view().inject(this);
         super.onCreate(savedInstanceState);
         EventBus.getDefault().register(this);
@@ -42,14 +41,51 @@ public class Activity_Group extends ActivityGroup{
 
     @Event(value = R.id.bottom,type = View.OnClickListener.class)
     private void bottom(View view){
-        content.removeAllViews();
-        myview=getLocalActivityManager().startActivity(
-                "Module1",
-                new Intent(Activity_Group.this,Activity_Player.class)//FLAG_ACTIVITY_CLEAR_TOP
-                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP ))//FLAG_ACTIVITY_BROUGHT_TO_FRONT
-                .getDecorView();
-        myview.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
-        content.addView(myview);
+//        content.removeAllViews();
+//        myview=getLocalActivityManager().startActivity(
+//                "Module1",
+//                new Intent(Activity_Group.this,Activity_Player.class)//FLAG_ACTIVITY_CLEAR_TOP
+//                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP ))//FLAG_ACTIVITY_BROUGHT_TO_FRONT
+//                .getDecorView();
+//        myview.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+//        content.addView(myview);
+
+        MediaTag tag =  MediaTag.createMediaTag("/mnt/sdcard/aaa.flac",true);
+        L.i("tag:"+tag.getAlbum());
+        L.i("tag:"+tag.getArtist());
+        L.i("tag:"+tag.getComment());
+        L.i("tag:"+tag.getGenre());
+        L.i("tag:"+tag.getTitle());
+        L.i("tag:"+tag.bitRate());
+        L.i("tag:"+tag.channels());
+        L.i("tag:"+tag.duration());
+        L.i("tag:"+tag.track());
+        L.i("tag:"+tag.year());
+        L.i("tag:"+tag.sampleRate());
+        L.i("tag:"+tag.cover());
+        tag.setAlbum("测试");
+        tag.setComment("这是注释");
+        tag.save();
+        L.i("tag----------------------------------");
+        L.i("tag:"+tag.getAlbum());
+        L.i("tag:"+tag.getArtist());
+        L.i("tag:"+tag.getComment());
+        L.i("tag:"+tag.getGenre());
+        L.i("tag:"+tag.getTitle());
+        L.i("tag:"+tag.bitRate());
+        L.i("tag:"+tag.channels());
+        L.i("tag:"+tag.duration());//
+        L.i("tag:"+tag.track());
+        L.i("tag:"+tag.year());
+        L.i("tag:"+tag.sampleRate());
+        L.i("tag:"+tag.cover());
+         tag.save();
+         tag.close();
+
+
+
+
+
     }
 
 
