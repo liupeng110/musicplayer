cd19ad3e4ac8b84d7d2b7a7cfe754473c358d9a7
liupeng110 <717219917@qq.com>
2017-10-12 16:14:52 +0800

    测试java

---
+++ lib/src/main/java/com/example/MyClass.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class MyClass {
+    public  static void main(String args[]){
+     test();
+    }
+
+    private  static void  test(){
+        for (int a=0;a<11;a++)
+        {
+            System.out.print("java:"+a+"\n");
+        }
+    }
+
+}
+
+
+
+
