6a2551da237593b6cbf6125ec7857c4fe949772d
liupeng110 <717219917@qq.com>
2017-10-23 18:11:44 +0800

    home后,通知栏拉起activity在oppo 中延迟大 需重构

---
+++ app/src/main/java/com/andlp/musicplayer/info.java
@@ -0,0 +1,19 @@
+package com.andlp.musicplayer;
+
+/**
+ * Created by Administrator on 2017/10/23.
+ */
+
+public class info {
+//通知栏逻辑
+//1.点击播放(底部play/list_play) 发出通知 并更新通知栏状态
+//2.1    点击通知栏 按钮  发出广播  NotifiReceiver接收后  分发给service处理 播放暂停等逻辑  并发消息更新ui中信息
+// 不管ui是否销毁
+ //2.2   点击logo唤起activity ,未注销  直接拉起.已注销 重新打开
+
+
+
+
+
+
+}
